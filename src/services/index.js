416fccf4c3ebc84194e5bf950703636d784fd213
Robin Ury <r.ury@tuta.io>
2017-04-22 02:57:42 -0500

    Added API route for users and challenges

--- src/services/index.js
+++ src/services/index.js
@@ -1,3 +1,7 @@
+const challenge = require('./challenge/challenge.service.js');
+const user = require('./user/user.service.js');
 module.exports = function () {
   const app = this; // eslint-disable-line no-unused-vars
+  app.configure(challenge);
+  app.configure(user);
 };
