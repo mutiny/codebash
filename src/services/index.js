d28fedfba43f20f74a40b134792a77cae720a02d
Robin Ury <r.ury@tuta.io>
2017-04-25 06:48:06 -0500

    Regenerated scaffolding with intent to utilize MongoDB purely rather than neDB as no foreseeable need for browser DB

--- src/services/index.js
+++ src/services/index.js
@@ -1,7 +1,3 @@
-const challenge = require('./challenge/challenge.service.js')
-const users = require('./users/users.service.js')
 module.exports = function () {
-  const app = this // eslint-disable-line no-unused-vars
-  app.configure(challenge)
-  app.configure(users)
-}
+  const app = this; // eslint-disable-line no-unused-vars
+};
