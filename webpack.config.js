fc8fb35a66b3601153e000364f3a6b836dc0cb9c
acrunchberry <jasong84@gmail.com>
2017-04-28 21:19:20 -0500

    Added SCSS files

--- webpack.config.js
+++ webpack.config.js
@@ -2,53 +2,49 @@ const path = require('path')
 const webpack = require('webpack')
 
 module.exports = {
-<<<<<<< HEAD
-    entry: path.resolve(__dirname, 'assets/js/index.js'),
-    output: {
-        filename: 'app.js',
-        path: path.resolve(__dirname, 'public')
-    },
+        entry: path.resolve(__dirname, 'assets/js/index.js'),
+        output: {
+            filename: 'app.js',
+            path: path.resolve(__dirname, 'public')
+        },
 
-    module: {
-        rules: [{
-            test: /\.js$/,
-            exclude: /(node_modules|bower_components)/,
-            use: {
-                loader: 'babel-loader',
-                options: {
-                    presets: ['env']
+        module: {
+            rules: [{
+                test: /\.js$/,
+                exclude: /(node_modules|bower_components)/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['env']
+                    }
                 }
-            }
-        }]
-    },
-    plugins: [
-        new webpack.ProvidePlugin({
-            $: 'jquery',
-            jQuery: 'jquery'
-        }),
-        new webpack.optimize.UglifyJsPlugin()
-=======
-  entry: path.resolve(__dirname, 'assets/js/index.js'),
-  output: {
-    filename: 'app.js',
-    path: path.resolve(__dirname, 'public')
-  },
-  module: {
-    rules: [
-      {
-        test: /\.js$/,
-        exclude: /(node_modules|bower_components)/,
-        use: {
-          loader: 'babel-loader',
-          options: {
-            presets: ['env']
-          }
-        }
-      },
-      {
-        test: /\.css$/,
-        loader: 'style-loader!css-loader'
-      }
->>>>>>> 4c5e6b64d16fac4c1f5d0a46636b5ab75904d4bf
-    ]
-}
+            }]
+        },
+        plugins: [
+                new webpack.ProvidePlugin({
+                    $: 'jquery',
+                    jQuery: 'jquery'
+                }),
+                new webpack.optimize.UglifyJsPlugin(),
+                entry: path.resolve(__dirname, 'assets/js/index.js'),
+                output: {
+                    filename: 'app.js',
+                    path: path.resolve(__dirname, 'public')
+                },
+                module: {
+                    rules: [{
+                            test: /\.js$/,
+                            exclude: /(node_modules|bower_components)/,
+                            use: {
+                                loader: 'babel-loader',
+                                options: {
+                                    presets: ['env']
+                                }
+                            }
+                        },
+                        {
+                            test: /\.css$/,
+                            loader: 'style-loader!css-loader'
+                        }
+                    ]
+                }
