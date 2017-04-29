78931f3010fd2090d4880fafaed344746f8679a1
acrunchberry <jasong84@gmail.com>
2017-04-28 20:13:00 -0500

    no changes to webpack file

--- webpack.config.js
+++ webpack.config.js
@@ -2,30 +2,29 @@ const path = require('path')
 const webpack = require('webpack')
 
 module.exports = {
-  entry: path.resolve(__dirname, 'assets/js/index.js'),
-  output: {
-    filename: 'app.js',
-    path: path.resolve(__dirname, 'public')
-  },
-  module: {
-    rules: [
-      {
-        test: /\.js$/,
-        exclude: /(node_modules|bower_components)/,
-        use: {
-          loader: 'babel-loader',
-          options: {
-            presets: ['env']
-          }
-        }
-      }
+    entry: path.resolve(__dirname, 'assets/js/index.js'),
+    output: {
+        filename: 'app.js',
+        path: path.resolve(__dirname, 'public')
+    },
+
+    module: {
+        rules: [{
+            test: /\.js$/,
+            exclude: /(node_modules|bower_components)/,
+            use: {
+                loader: 'babel-loader',
+                options: {
+                    presets: ['env']
+                }
+            }
+        }]
+    },
+    plugins: [
+        new webpack.ProvidePlugin({
+            $: 'jquery',
+            jQuery: 'jquery'
+        }),
+        new webpack.optimize.UglifyJsPlugin()
     ]
-  },
-  plugins: [
-    new webpack.ProvidePlugin({
-      $: 'jquery',
-      jQuery: 'jquery'
-    }),
-    new webpack.optimize.UglifyJsPlugin()
-  ]
 }
